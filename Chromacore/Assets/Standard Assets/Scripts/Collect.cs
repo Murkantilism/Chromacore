dfa6d2b69a687f49b55bf83b2de34b7b64548a04
Deniz Ozkaynak <deniz.ozkaynak@gmail.com>
2013-09-27 15:32:39 -0400

    Initial Commit.

---
+++ Chromacore/Assets/Standard Assets/Scripts/Collect.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void Pickup(AudioClip sound)
+	{
+		//AudioSource.PlayClipAtPoint(sound, transform.position);
+		Debug.Log("Play sound");
+	}
+}
---
+++ Chromacore/Assets/Standard Assets/Scripts/Pickup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+	
+	public AudioClip collectSound;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.tag == "Player")
+		{
+			gameObject.SendMessage("Pickup", collectSound);
+			Destroy(gameObject);
+		}
+	}
+		
+}
