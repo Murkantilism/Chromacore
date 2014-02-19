cb0dc9b5977ff3f30734ae66ee4c5a7e760b7313
Deniz Ozkaynak <deniz.ozkaynak@gmail.com>
2014-02-19 01:49:55 -0500

    Implemented Automation Tool for Note Assignment
    NoteAssigner.cs grabs an array of mp3 files from the Resources/ folder
    and assigns them to their respective Note gameObjects. Big time
    saver!!!!

---
+++ Chromacore/Assets/Standard Assets/Scripts/Note Placement/NoteAssigner.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+// An automation tool used to assign pick-up mp3s to their respective Notes
+public class NoteAssigner : MonoBehaviour {
+
+	// An array of Notes
+	public GameObject[] NotesArray;
+	
+	// A List of Notes
+	public List<GameObject> Notes;
+
+	// Do we need to reset the Notes (For editor purposes ONLY!)
+	public bool editorResetP = false;
+
+	public AudioClip[] pickupMP3s;
+
+	// Use this for initialization
+	void Start () {
+		// Grab and sort array of Notes
+		NotesArray = GameObject.FindGameObjectsWithTag("Note");
+		SortNotes();
+		
+		// Convert array to List (in order to place Notes easily)
+		Notes = NotesArray.OfType<GameObject>().ToList();
+		Debug.Log("After List Conversion: " + Notes.Count);
+
+		// Grab a list of pick-up mp3 files
+		pickupMP3s = Resources.LoadAll<AudioClip>("Level4/Level4_Pickup_Tracks");
+		//pickupMP3s = Resources.LoadAll("Level4/Level4_Pickup_Tracks") as AudioClip[];
+	}
+
+	// Sort the List of Notes in numerical order
+	void SortNotes(){
+		// If we need to reset first, add "Note " prefix
+		if (editorResetP == true){
+			foreach (GameObject note in NotesArray){
+				try{ 
+					note.name = note.name.Insert(0, "Note");
+				}catch(Exception e){
+					Debug.Log(e.ToString());
+				}
+			}
+			// Once the reset is done, set it to false
+			editorResetP = false;
+		}
+		
+		// For each note in the List of Notes
+		foreach (GameObject note in NotesArray){
+			// Slice off the first four characters from every name of each Note gameobject
+			// Ex: "Note34" becomes "34" - making numerical sorting possible
+			try{ 
+				note.name = note.name.Substring(4);
+			}catch(Exception e){
+				Debug.Log(e.ToString());
+			}
+		}
+		
+		// Actually sort the notes
+		Array.Sort(NotesArray, sortList);
+		
+		// Call PlaceNotes
+		AssignNotes();
+	}
+
+	int sortList(GameObject a, GameObject b){
+		return int.Parse(a.name) - int.Parse(b.name);
+	}
+
+	// Assign each mp3 file to a Note
+	void AssignNotes(){
+		for (int i = 0; i < NotesArray.Length; i++){
+			foreach (GameObject note in NotesArray){
+				try{
+					NotesArray[i].audio.clip = pickupMP3s[i];
+					//Debug.Log(pickupMP3s[i]);
+					//Debug.Log(note.audio.clip.name);
+				}catch(Exception e){
+					Debug.Log(e.ToString());
+				}
+			}
+		}
+		
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
