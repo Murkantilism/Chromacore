cfb310aeb93bd08cacc0bf4757293fd24b48d044
Deniz Ozkaynak <deniz.ozkaynak@gmail.com>
2014-02-24 14:35:14 -0500

    Added y position to AuomaticNotePlacer.cs
    -AutomaticNotePlacer now takes care of y positions as well (by
    raycasting downwards and checking the proximity of Notes to platforms).
    Notes not above a platform are flagged for manual attention.

--- Chromacore/Assets/Standard Assets/Scripts/Note Placement/AutomaticNotePlacer.cs
+++ Chromacore/Assets/Standard Assets/Scripts/Note Placement/AutomaticNotePlacer.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Collections.Generic;
 
 [ExecuteInEditMode]
+// Purpose: Auomatically place Notes in the correct x positions
 public class AutomaticNotePlacer : MonoBehaviour {
 	// Purpose: To automatically place Note objects at the 
 	// X postions calculated by xPositionCalcular.cs
@@ -84,17 +85,53 @@ public class AutomaticNotePlacer : MonoBehaviour {
 
 	// Assign each note in this array of Notes a corresponding x-position
 	void PlaceNotes(){
-		// For every Note in Notes
+		RaycastHit hit;
+
+		// Minimum and maximum threshold for y distance
+		// from Note positon to level platform postion.
+		float yThreshMin = 0.75f;
+		float yThreshMax = 3.0f;
+
+		// Min and max threshold for y distance from Note
+		// position to level platform when Notes aren't
+		// above any platforms.
+		float offPlatform_yThreshMin = 5.0f;
+		float offPlatform_yThreshMax = 7.0f;
+
+		// For each Note in Notes
+		foreach(GameObject note in Notes){
+			// Draw a ray downwards in the y direction
+			if(Physics.Raycast(note.transform.position, Vector3.down, out hit, 10)){
+				Debug.Log(note.name + " : " + hit.collider.name + " : " + hit.distance);
+
+				// If this Note's y position is below the min treshold
+				if (hit.distance < yThreshMin){
+					// Increment this Note's y poition to the treshold
+					note.transform.position = new Vector3(note.transform.position.x, note.transform.position.y + (yThreshMin - hit.distance), note.transform.position.z);
+					Debug.Log("ATTN: Note " + note.name + "'s y position has been INcrememnted by " + note.transform.position.y + (yThreshMin - hit.distance));
+				// Elif this Note's y position is above the max threshold AND below
+				// the minimum off-platform threshold
+				}else if(hit.distance > yThreshMax && hit.distance < offPlatform_yThreshMin){
+					// Decrement this Note's y position to the Max treshold
+					note.transform.position = new Vector3(note.transform.position.x, note.transform.position.y - (yThreshMax - hit.distance), note.transform.position.z);
+					Debug.Log("ATTN: Note " + note.name + "'s y position has been DEcrememnted by " + (note.transform.position.y - (yThreshMax - hit.distance)));
+				// Elif this Note's y position above the max off-platform treshold
+				}else if(hit.distance > offPlatform_yThreshMax){
+					// Flag this Note for manual placement
+					Debug.LogError("ATTN: Note " + note.name + " requires manual y-positon placement.");
+				}
+			}
+		}
+
+		// For every Note in Notes convert the xPos in the List from 
+		// string to float, then assign the x-postion to each note 
+		// in Notes, leaving the y & z positions alone.
 		for(int i = 0; i < Notes.Count; i++){
-			// Convert the xPos in the List from string to float,
-			// then assign the x-postion to each note in Notes,
-			// leaving the y & z positions alone.
 			try{
 				Notes[i].transform.position = new Vector3(float.Parse(myXPositions[i]), Notes[i].transform.position.y, Notes[i].transform.position.z);
 			}catch (Exception e){
 				Debug.Log(e.ToString());
 			}
-			//Debug.Log(Notes[i].transform.position.ToString());
 		}
 	}
 	
