8e94a162adbf46177f0c3f6a66626b9fe22971ed
Deniz Ozkaynak <deniz.ozkaynak@gmail.com>
2014-03-04 12:16:24 -0500

    Polished levels 2, 4, 5 (final chunk of commit)

---
+++ Chromacore/Assets/Standard Assets/Scripts/Note Placement/TestNote.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestNote : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.tag == "Player"){
+			Debug.Log("TIME: " + Time.timeSinceLevelLoad);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
