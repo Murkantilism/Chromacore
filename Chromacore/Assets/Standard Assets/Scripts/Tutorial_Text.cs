fa37da23a48585ddafc6f3eaeab13eca34c845f9
Deniz Ozkaynak <deniz.ozkaynak@gmail.com>
2014-03-12 01:29:03 -0400

    Tutorial is now more explicit
    The tutorial in level 1 now explicitly introduces Note pickup mechanics
    of playing part of music track & changing bg color

--- Chromacore/Assets/Standard Assets/Scripts/Tutorial_Text.cs
+++ Chromacore/Assets/Standard Assets/Scripts/Tutorial_Text.cs
@@ -7,6 +7,8 @@ public class Tutorial_Text : MonoBehaviour {
 	public TextMesh tutorialTextJump;
 	public TextMesh tutorialTextPunch;
 	public TextMesh tutorialTextPause;
+	public TextMesh tutorialTextNote_Music;
+	public TextMesh tutorialTextNote_Background;
 
 	// Use this for initialization
 	void Start () {
@@ -16,12 +18,16 @@ public class Tutorial_Text : MonoBehaviour {
 		tutorialTextJump.text = "Press 'space' to jump";
 		tutorialTextPunch.text = "Press 'A' to punch";
 		tutorialTextPause.text = "Press 'ESC' to pause";
+		tutorialTextNote_Music.text = "Notes will play part \n of the music track";
+		tutorialTextNote_Background.text = "Notes will also add \n color to background!";
 		#endif
 		
 		#if UNITY_IPHONE
 		tutorialTextJump.text = "Tap screen to jump";
 		tutorialTextPunch.text = "2 fingers to punch";
 		tutorialTextPause.text = "3 fingers to pause";
+		tutorialTextNote_Music.text = "Notes will play \n part of music track";
+		tutorialTextNote_Background.text = "Notes will add \n color to background";
 		#endif
 
 		#if UNITY_ANDROID
@@ -31,6 +37,8 @@ public class Tutorial_Text : MonoBehaviour {
 				tutorialTextJump.text = "Press A to jump";
 				tutorialTextPunch.text = "Press X to punch";
 				tutorialTextPause.text = "Press B to pause";
+				tutorialTextNote_Music.text = "Notes will play \n part of music track";
+				tutorialTextNote_Background.text = "Notes will add \n color to background";
 			}
 		// Otherwise assume it's a touchscreen
 		}catch(Exception e){
@@ -38,6 +46,8 @@ public class Tutorial_Text : MonoBehaviour {
 			tutorialTextJump.text = "Tap screen to jump";
 			tutorialTextPunch.text = "2 fingers to punch";
 			tutorialTextPause.text = "3 fingers to pause";
+			tutorialTextNote_Music.text = "Notes will play \n part of music track";
+			tutorialTextNote_Background.text = "Notes will add \n color to background";
 		}
 		#endif
 	}
