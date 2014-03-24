84689f3f0c1537e013cc5bb7609ea44003d3cf8f
Deniz Ozkaynak <deniz.ozkaynak@gmail.com>
2014-03-24 19:01:45 -0400

    Added Credits screen with HTML linking

---
+++ Chromacore/Assets/Standard Assets/Scripts/Menus/creditsScreen.js
@@ -0,0 +1,61 @@
+﻿#pragma strict
+
+var deniz = false;
+var jeff = false;
+var korbin = false;
+var jen = false;
+var ronny = false;
+var andre = false;
+var bj = false;
+var cheyenne = false;
+var casper = false;
+var joshua = false;
+
+var defaultURL = "https://www.facebook.com/incendiaryindustries";
+
+function Start () {
+
+}
+
+function OnMouseEnter(){
+	// Change the color of the text
+	renderer.material.color = Color.blue;
+}
+
+function OnMouseExit(){
+	// Change the color of the text back to the original color (white)
+	renderer.material.color = Color.white;
+}
+
+function OnMouseUp(){
+	if (deniz){
+		Application.OpenURL("http://www.ccs.neu.edu/home/ozkaynak/");
+	}else if(jeff){
+		Application.OpenURL(defaultURL);
+	}else if(korbin){
+		Application.OpenURL(defaultURL);
+	}else if(jen){
+		Application.OpenURL(defaultURL);
+	}else if(ronny){
+		Application.OpenURL(defaultURL);
+	}else if(andre){
+		Application.OpenURL(defaultURL);
+	}else if(bj){
+		Application.OpenURL(defaultURL);
+	}else if(cheyenne){
+		Application.OpenURL(defaultURL);
+	}else if(casper){
+		Application.OpenURL(defaultURL);
+	}else if(joshua){
+		Application.OpenURL(defaultURL);
+	}
+}
+
+function OnGUI() {
+	var buttonStyle = new GUIStyle("button");
+	buttonStyle.fontSize = 25;
+	
+	if (GUI.Button(new Rect(Screen.width/2 + Screen.width/4, Screen.height/2 + Screen.height/4, 200, 100), "Back", buttonStyle)){
+		Application.LoadLevel("MainMenu");
+	}
+}
--- Chromacore/Assets/Standard Assets/Scripts/Menus/mainMenu.js
+++ Chromacore/Assets/Standard Assets/Scripts/Menus/mainMenu.js
@@ -4,7 +4,7 @@
 // Contact:   incendiaryindustries@gmail.com   with any questions
 
 var quitButtonp = false;
-var optionsButtonp = false;
+var creditsButtonp = false;
 var autoGeneratorp = false;
 var playButtonp = false;
 
@@ -22,9 +22,9 @@ function OnMouseUp(){
 	if(quitButtonp){
 		//If the quit button is clicked, quit the game
 		Application.Quit();
-	}else if(optionsButtonp){
-		//If the Options button is clicked, open the options menu
-		//Application.LoadLevel("OptionsScreen");
+	}else if(creditsButtonp){
+		//If the Credits button is clicked, open the Credits screen
+		Application.LoadLevel("CreditsScreen");
 	}else if(autoGeneratorp){
 		// If the Level Auto-Generator is picked, load the file brwoser
 		Application.LoadLevel("SongBrowser");
