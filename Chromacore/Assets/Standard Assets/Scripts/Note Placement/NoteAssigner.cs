fb6c2b6d924d0a2190124349fbe305838c137f9d
Deniz Ozkaynak <deniz.ozkaynak@gmail.com>
2014-04-08 04:11:25 -0400

    Added Level 12
    -Level 12 added with automated tools, have yet to design it

--- Chromacore/Assets/Standard Assets/Scripts/Note Placement/NoteAssigner.cs
+++ Chromacore/Assets/Standard Assets/Scripts/Note Placement/NoteAssigner.cs
@@ -34,7 +34,7 @@ public class NoteAssigner : MonoBehaviour {
 
 		// Grab a list of pick-up mp3 files
 		//EDITME
-		pickupMP3s = Resources.LoadAll<AudioClip>("Level11/Level11_pickupTracks");
+		pickupMP3s = Resources.LoadAll<AudioClip>("Level12/Level12_pickupTracks");
 	}
 
 	// Sort the List of Notes in numerical order
