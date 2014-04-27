e1c639bae04017edbc7a2e07f644a4b18549eaf8
Deniz Ozkaynak <deniz.ozkaynak@gmail.com>
2014-04-26 20:28:05 -0400

    Added script to randomize Note animations

---
+++ Chromacore/Assets/Standard Assets/Scripts/Note Placement/RotateNotes.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+// Used to assign Notes a random Note animation to give them variety
+public class RotateNotes : MonoBehaviour {
+
+	// The animation for this Note
+	public tk2dSpriteAnimator noteAnimation;
+
+	// A list of possible animations
+	tk2dSpriteAnimationClip[] listOfClips;
+
+
+	// Use this for initialization
+	void Start () {
+		// Get the animator attached to this Note
+		noteAnimation = GetComponent<tk2dSpriteAnimator>();
+		// Get the list of possible animations
+		listOfClips = noteAnimation.Library.clips;
+
+		AssignAnimation();
+	}
+
+	// Assign each Note a random animation
+	void AssignAnimation(){
+		int randomNum = UnityEngine.Random.Range(0, listOfClips.Length);
+		noteAnimation.Play(listOfClips[randomNum]);
+	}
+}
--- Chromacore/Assets/Standard Assets/Scripts/Pickup.cs
+++ Chromacore/Assets/Standard Assets/Scripts/Pickup.cs
@@ -1,5 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 
 public class Pickup : MonoBehaviour {
 	
@@ -12,9 +16,28 @@ public class Pickup : MonoBehaviour {
 	
 	public GameObject particleEffect;
 	
+	// The animation for this Note
+	public tk2dSpriteAnimator noteAnimation;
+	
+	// A list of possible animations
+	tk2dSpriteAnimationClip[] listOfClips;
+	
 	// Use this for initialization
 	void Start () {
 		parent = transform.parent.gameObject;
+
+		// Get the animator attached to this Note
+		noteAnimation = GetComponent<tk2dSpriteAnimator>();
+		// Get the list of possible animations
+		listOfClips = noteAnimation.Library.clips;
+		
+		AssignAnimation();
+	}
+
+	// Assign each Note a random animation
+	void AssignAnimation(){
+		int randomNum = UnityEngine.Random.Range(0, listOfClips.Length);
+		noteAnimation.Play(listOfClips[randomNum]);
 	}
 	
 	// Upon picking up this object, trigger events
