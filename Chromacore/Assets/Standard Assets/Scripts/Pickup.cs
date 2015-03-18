c26965f224649edd8df768bbe1ca173155b8e886
Catalin Craciun <catalincraciun7@yahoo.com>
2015-03-18 16:28:58 +0200

    Notes have been changed to mystic balls - Graphics changed

--- Chromacore/Assets/Standard Assets/Scripts/Pickup.cs
+++ Chromacore/Assets/Standard Assets/Scripts/Pickup.cs
@@ -6,26 +6,20 @@ public class Pickup : MonoBehaviour {
 	// Corresponding sound to the musical track attached to 
 	// the object
 	private AudioClip collectSound;
-	
-	// Get the parent of this collectible
-	GameObject parent;
-	
+
 	// Use this for initialization
 	void Start () {
-		parent = transform.parent.gameObject;
+
 	}
 	
 	// Upon picking up this object, trigger events
 	void OnTriggerEnter(Collider col){
-		Debug.Log ("i am on " + gameObject);
 		if(col.gameObject.tag == "Player")
 		{
-			// Change the color of the textures on pickup
-			parent.SendMessage("ChangeColor");
-			// A note has been collected so increment the score
-			col.SendMessage("CollectNote");
+			Debug.Log ("The player took a mistic ball");
+
 			// Make this object invisible
-			gameObject.GetComponent<Renderer>().enabled = false;
+			GetComponent<Renderer>().enabled = false;
 			// Play the corresponding sound
 			GetComponent<AudioSource>().Play();
 		}
