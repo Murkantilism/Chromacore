496fc48c19f2470138bcead412a7139d6bb0020a
Catalin Craciun <catalincraciun7@yahoo.com>
2015-03-26 23:14:48 +0200

    Minor changes

--- Chromacore/Assets/Scripts/SceneManagement.cs
+++ Chromacore/Assets/Scripts/SceneManagement.cs
@@ -3,20 +3,17 @@ using System.Collections;
 
 public class SceneManagement : MonoBehaviour {
 
-	public Transform struct1;
-	public Transform struct2;
-	public Transform struct3;
-	public Transform struct4;
-	public Transform struct5;
-	public Transform struct6;
-	public Transform struct7;
+	public GameObject struct1;
+	public GameObject struct2;
+	public GameObject struct3;
+	public GameObject struct4;
+	public GameObject struct5;
+	public GameObject struct6;
+	public GameObject struct7;
 
 	// Use this for initialization
 	void Start () {
-		for (int i=1; i<=3; i++) {
-			Transform test = (Transform)(Instantiate (struct1, new Vector3 ((float)(75 * (i - 1)), 0f, 0f), Quaternion.identity));
-			Destroy (test.gameObject, (float)(10*i));
-		}
+
 	}
 	
 	// Update is called once per frame
