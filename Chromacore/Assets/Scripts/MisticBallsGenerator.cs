3a1c94c6fe57fda124c3a1b6d7f7cbe9daca489b
Catalin Craciun <catalincraciun7@yahoo.com>
2015-03-28 14:20:46 +0200

    Fixed bug in mistic balls generation

--- Chromacore/Assets/Scripts/MisticBallsGenerator.cs
+++ Chromacore/Assets/Scripts/MisticBallsGenerator.cs
@@ -9,13 +9,20 @@ public class MisticBallsGenerator : MonoBehaviour {
 	public int maximumPoints;
 
 	public Vector2[] points;
+	bool[] used;
 
 	// Use this for initialization
 	void Start () {
 		int pointsToGenerate = (int)Random.Range (minimumPoints, maximumPoints+1);
+		used = new bool[25];
+		for (int i = 0; i <= points.Length; i++)
+			used [i] = false;
 		for (int i = 1; i <= pointsToGenerate; i++) {
 			int index = Random.Range (0, points.Length);
+			while (used[index] == true)
+				index = Random.Range (0, points.Length);
 			(Instantiate (misticBall, new Vector3(this.transform.position.x + points[index].x, this.transform.position.y + points[index].y, 0f), Quaternion.identity) as GameObject).transform.parent = this.transform;
+			used[index] = true;
 		}
 	}
 }
