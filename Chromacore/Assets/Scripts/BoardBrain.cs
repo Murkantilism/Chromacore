756c18e9494276e4a2b2259626bd419480bba66f
Catalin Craciun <catalincraciun7@yahoo.com>
2015-04-08 11:11:47 +0300

    Some changes in UI - There is still work to do #revolutionHasStarted

---
+++ Chromacore/Assets/Scripts/BoardBrain.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardBrain : MonoBehaviour {
+
+	GameObject mainCamera;
+	GUIText scoreLabel;
+	Renderer boardRenderer;
+
+	bool shouldUpdate;
+
+	void InitGUI(string labelToSet) {
+		shouldUpdate = true;
+		scoreLabel.enabled = true;
+		boardRenderer.enabled = true;
+
+		scoreLabel.text = labelToSet;
+	}
+
+	// Use this for initialization
+	void Start () {
+		shouldUpdate = false;
+		boardRenderer = GetComponent<Renderer> ();
+		mainCamera = GameObject.FindGameObjectWithTag ("MainCamera");
+
+		GameObject scoreBoardGUI = GameObject.FindGameObjectWithTag ("ScoreBoardGUI");
+
+		scoreLabel = scoreBoardGUI.GetComponent<GUIText>();
+		scoreLabel.enabled = false;
+		boardRenderer.enabled = false;
+		scoreLabel.pixelOffset = new Vector2(Screen.width/2, Screen.height/2 + 20);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (shouldUpdate) {
+			gameObject.transform.parent.position = new Vector3 (mainCamera.transform.position.x, mainCamera.transform.position.y, -1);
+		}
+	}
+}
--- Chromacore/Assets/Scripts/ScoringSystem.cs
+++ Chromacore/Assets/Scripts/ScoringSystem.cs
@@ -4,12 +4,19 @@ using System.Collections;
 public class ScoringSystem : MonoBehaviour {
 
 	// Properties
+	GameObject scoreBoard;
+
 	GUIText scoreLabel;
 
 	private int score;
 	private const string highScoreKey = "HighScore";
 
 	// Methods
+	void ShowOnScreen() {
+		string labelToSet = "Score: " + score.ToString() + "\n\n" + "High Score: " + PlayerPrefs.GetInt (highScoreKey).ToString ();
+		scoreBoard.SendMessage ("InitGUI", labelToSet);
+	}
+
 	public void RegisterScore () {
 		int currentHighScore = PlayerPrefs.GetInt (highScoreKey);
 
@@ -21,7 +28,8 @@ public class ScoringSystem : MonoBehaviour {
 	
 		// Do what you want with the updated high score and the current score
 		// ...
-		scoreLabel.text = "High Score: " + PlayerPrefs.GetInt (highScoreKey);
+
+		Invoke ("ShowOnScreen", 1f);
 	}
 	
 	public void ResetScore() {
@@ -31,8 +39,10 @@ public class ScoringSystem : MonoBehaviour {
 	void Start () {
 		score = 0;
 
+		scoreBoard = GameObject.FindGameObjectWithTag ("ScoreBoard");
+
 		scoreLabel = GetComponent<GUIText>();
-		scoreLabel.pixelOffset = new Vector2(-(Screen.width/2) + 100, (Screen.height/2) - 50);
+		scoreLabel.pixelOffset = new Vector2(-(Screen.width/2) + 25, (Screen.height/2) - 50);
 	}
 	
 	// Update is called once per frame
